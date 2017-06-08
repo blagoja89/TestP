bfbc4087f953e53cb92e874fb8b231703c8bc4a0
blagoja89 <blagoja_tr@yahoo.com>
2017-06-08 19:10:25 +0200

    -added sample dotnet core projects

---
+++ TestP.ConsoleDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestP.ConsoleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
